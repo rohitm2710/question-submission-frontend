ed359c297360852da686e7d858da971e7d2c5e62
rohitm2710 <rohitmajumder05@gmail.com>
2026-09-25 11:39:02 +0530

    edits

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,7 +13,7 @@ const Login = ({ onLogin, onChangePassword }) => {
         method: 'POST',
         headers: { 
           'Content-Type': 'application/json',
-          'KGEC_IT_access': process.env.VERCEL_BYPASS_SECRET1 },
+          'x-vercel-protection-bypass': process.env.VERCEL_BYPASS_SECRET1 },
         body: JSON.stringify(credentials),
       });
 
